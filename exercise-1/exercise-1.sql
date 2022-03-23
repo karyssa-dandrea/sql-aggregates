789f51716bbab91c2e91d71b5b4ec9e01488dc96
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-23 12:41:53 -0700

    got two tests to pass

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,24 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+
+SELECT
+    title,
+COUNT (rental_date)
+FROM
+    film
+INNER JOIN
+	inventory
+ON
+	film.film_id = inventory.film_id
+INNER JOIN
+	rental
+ON
+	rental.inventory_id = inventory.inventory_id
+GROUP BY
+	film.film_id
+ORDER BY
+	count DESC
+LIMIT
+	10
